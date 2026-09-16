6923a9f726de9778ff887a7854077ce88e58cd3f
Eliah <eliahdanielle777@gmail.com>
2026-09-16 19:26:42 +0200

    Minor Changes to Apply_Shot() Function

--- battleship-fleet/battleship_final.py
+++ battleship-fleet/battleship_final.py
@@ -255,13 +255,18 @@ def apply_shot(state,cell):
                         fleet_defeated = False 
                         #stop checking the remaining ships 
                         break 
-                #The sunk result and fleet status. 
-                return {"result":f"sunk:{ship}","fleet_defeated":fleet_defeated} 
+                 # Check if the ship has sunk but the fleet is not defeated.
+                if not fleet_defeated:
+                    return {"result":f"sunk:{ship}","fleet_defeated":False}
+
+                #The sunk result and fleet status when every ship is defeated.
+                return {"result":f"sunk:{ship}","fleet_defeated":True}
+
             #a normal hit because the ship still has cells remaining 
             return {"result":"hit","fleet_defeated":False} 
+
     #return miss when the shot did not hit any ship. 
     return{"result":"miss","fleet_defeated":False}
-
 # This function checks whether every ship in the player's fleet has been completely hit.[Hannah]
 def all_ships_sunk(state):
 
